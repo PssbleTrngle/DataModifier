42318fd5416d535e11c7f6a78e7db38d2f326b07
Niklas Widmann <nik@somethingcatchy.net>
2023-10-09 17:09:26 +0200

    coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
    preset: 'ts-jest/presets/default-esm',
-   rootDir: 'test',
    extensionsToTreatAsEsm: ['.ts'],
    testEnvironment: 'node',
-   setupFilesAfterEnv: ['<rootDir>/setup.ts'],
+   setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
    transform: {
       '^.+\\.ts$': [
          'ts-jest',
