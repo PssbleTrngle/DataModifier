99fce207a28607c4271f127a76c07757d78fa8a4
Niklas Widmann <nik@somethingcatchy.net>
2023-11-07 22:26:31 +0100

    re-add default stub files

---
+++ @types/stub/index.d.ts
@@ -0,0 +1,13 @@
+declare module "@pssbletrngle/data-modifier/generated" {
+  export type RegistryId = string;
+
+  export type InferIds<T extends RegistryId> = string;
+
+  export type ItemId = string;
+
+  export type BlockId = string;
+
+  export type FluidId = string;
+
+  export type RecipeSerializerId = string;
+}
